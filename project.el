f4caa54b5b59ea85a80ab24f38c024c4ea1ff5fe
Johan <johan.felisaz@protonmail.com>
2020-04-13 19:21:06 +0200

    initial commit

---
+++ project.el
@@ -0,0 +1,24 @@
+(require 'ox-publish)
+
+(setq org-publish-project-alist
+      '(("org-notes"
+	 :base-directory "org/"
+	 :base-extension "org"
+	 :publishing-directory "./"
+	 :recursive t
+	 :publishing-function org-html-publish-to-html
+
+	 ;; All the style stuff here
+	 :html-head "<link rel=\"stylesheet\" href=\"css/style.css\" type=\"text/css\"/>"
+	 :with-toc nil
+	 :section-numbers nil
+	 :html-postamble nil)
+
+	("org-static"
+	 :base-directory "org/"
+	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+	 :publishing-directory "./"
+	 :recursive t
+	 :publishing-function org-publish-attachment)
+
+	("org" :components ("org-notes" "org-static"))))
