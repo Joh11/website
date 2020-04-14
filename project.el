0625d795fa1da94a7ab5bd71fba216978fcaa786
Johan <johan.felisaz@protonmail.com>
2020-04-14 09:37:51 +0200

    improve css import (with absolute paths)

--- project.el
+++ project.el
@@ -1,23 +1,26 @@
 (require 'ox-publish)
 
+(setq joh/website/base-dir (concat default-directory "org/"))
+(setq joh/website/publish-dir default-directory)
+
 (setq org-publish-project-alist
-      '(("org-notes"
-	 :base-directory "org/"
+      `(("org-notes"
+	 :base-directory ,joh/website/base-dir
 	 :base-extension "org"
-	 :publishing-directory "./"
+	 :publishing-directory ,joh/website/publish-dir
 	 :recursive t
 	 :publishing-function org-html-publish-to-html
 
 	 ;; All the style stuff here
-	 :html-head "<link rel=\"stylesheet\" href=\"css/style.css\" type=\"text/css\"/>"
+	 :html-head ,(format "<link rel=\"stylesheet\" href=\"%scss/style.css\" type=\"text/css\"/>" joh/website/base-dir)
 	 :with-toc nil
 	 :section-numbers nil
 	 :html-postamble nil)
 
 	("org-static"
-	 :base-directory "org/"
+	 :base-directory ,joh/website/base-dir
 	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-	 :publishing-directory "./"
+	 :publishing-directory ,joh/website/publish-dir
 	 :recursive t
 	 :publishing-function org-publish-attachment)
 
