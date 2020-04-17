02531dd762b9807372135a000e821df0ea4ae72b
Johan <johan.felisaz@protonmail.com>
2020-04-17 18:54:56 +0200

    add nav bar

--- project.el
+++ project.el
@@ -8,7 +8,7 @@
     (buffer-string)))
 
 (setq joh/website/base-dir (concat default-directory "org/"))
-(setq joh/website/publish-dir default-directory)
+(setq joh/website/publish-dir default-directory) ;; TODO fix the absolute path stuff 
 (setq joh/website/template-dir (concat default-directory "org/templates/"))
 
 (setq org-publish-project-alist
@@ -20,7 +20,9 @@
 	 :publishing-function org-html-publish-to-html
 
 	 ;; All the style stuff here
-	 :html-head ,(format "<link rel=\"stylesheet\" href=\"%scss/style.css\" type=\"text/css\"/>" joh/website/base-dir)
+	 :html-doctype "html5"
+	 
+	 :html-head "<link rel=\"stylesheet\" href=\"css/style.css\" type=\"text/css\"/>"
 
 	 :html-preamble ,(joh/get-string-from-file (concat joh/website/template-dir "preamble.html"))
 	 :html-postamble ,(joh/get-string-from-file (concat joh/website/template-dir "postamble.html"))
